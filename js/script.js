6986e2c0bdcfa44eea2f62d0a1302f76fae965c2
IhorStron <worklife0025@gmail.com>
2023-11-14 17:35:20 +0200

    added slider, section tours

--- js/script.js
+++ js/script.js
@@ -11,3 +11,53 @@ if (burger && menu) {
       body.classList.toggle('_lock');
    })
 }
+
+
+
+//---Swiper
+
+// const popularSlider = new Swiper('.tours-slider', {
+//    spaceBetween: 30,
+//    slidesPerView: 1,
+//    navigation: {
+//      nextEl: '.tours-slider-next',
+//      prevEl: '.tours-slider-prev',
+//    },
+//    breakpoints: {
+// 		992: {
+// 			slidesPerView: 3,
+// 		},
+// 		660: {
+// 			slidesPerView: 2,
+// 		}
+// 	}
+//  });
+
+
+
+
+const toursSlider = new Swiper('.tour-slide', {
+   spaceBetween: 30,
+   slidesPerView: 1,
+   navigation: {
+     nextEl: '.tours-slider-next',
+     prevEl: '.tours-slider-prev',
+   },
+   breakpoints: {
+		992: {
+			slidesPerView: 3,
+		},
+		660: {
+			slidesPerView: 2,
+		}
+	}
+ });
+
+
+
+
+
+
+
+
+
